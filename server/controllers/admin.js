4bbf3cebd3dcdaa18a5610dbe3674a0aad7f77b5
林一白 <linyibai23@gmail.com>
2018-01-31 17:40:57 +0800

    test edit api

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const fs = require('fs');
+const md = require('markdown-it')();
 
 exports.page = function(req, res) {
   const options = {
@@ -7,8 +9,21 @@ exports.page = function(req, res) {
   res.sendFile('admin.html', options);
 }
 
-exports.post = function(req, res) {
+exports.edit = function(req, res) {
+  const title = req.body.title;
   const post = req.body.post;
 
-  
+  console.log('文章标题：' + title);
+  console.log('提交的文章：' + post);
+
+  const content = md.render(post);
+
+  console.log('准备写入文件')
+
+  fs.writeFile(`${title}.md`, content, err => {
+  	if (err) return console.log(err);
+  	console.log("数据写入成功！");
+  })
+
+  res.send('<h1>发表成功!</h1>')
 }
